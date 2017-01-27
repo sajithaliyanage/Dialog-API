f76d11c522d05a8ccfbae9a1fc633bc0b44db0b8
Sajitha Liyanage <sajithaliyanage@gmail.com>
2017-01-28 00:58:47 +0530

    updated!

--- test_curl/1.php
+++ test_curl/1.php
@@ -1,29 +1,19 @@
 <?php
 
-//extract data from the post
-//set POST variables
-$url = 'https://drawningbunny.azurewebsites.net/test_curl/2.php';
-$fields = array(
-	'postvar1' => 1,
-	'postvar2' => 2
-	
-);
-
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');
-
-//open connection
-$ch = curl_init();
-
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, count($fields));
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-//execute post
-$result = curl_exec($ch);
-echo $result."@@@@";
-//close connection
-curl_close($ch);
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://drawningbunny.azurewebsites.net/test_curl/2.php',
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => array(
+        'postvar1' => 'value',
+        'postvar2' => 'value2'
+    )
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
 ?>
