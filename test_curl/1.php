effaccf4874d830847c221b0b6e903b5a549d5bc
Sajitha Liyanage <sajithaliyanage@gmail.com>
2017-01-28 00:53:02 +0530

    udated

--- test_curl/1.php
+++ test_curl/1.php
@@ -5,7 +5,7 @@
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"http://localhost/img/2.php");
+curl_setopt($ch, CURLOPT_URL,"https://drawningbunny.azurewebsites.net/test_curl/2.php");
 curl_setopt($ch, CURLOPT_POST, 2);
 curl_setopt($ch, CURLOPT_POSTFIELDS,
             "postvar1=1&postvar2=2");
@@ -15,7 +15,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS,
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $server_output = curl_exec ($ch);
-
+echo $server_output."===";
 curl_close ($ch);
 
 // further processing ....
