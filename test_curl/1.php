ede6f6234189bd69fe309b813d69587ae7ffbd48
Sajitha Liyanage <sajithaliyanage@gmail.com>
2017-01-28 00:56:24 +0530

    updated

--- test_curl/1.php
+++ test_curl/1.php
@@ -1,24 +1,29 @@
 <?php
-//
-// A very simple PHP example that sends a HTTP POST to a remote site
-//
 
+//extract data from the post
+//set POST variables
+$url = 'https://drawningbunny.azurewebsites.net/test_curl/2.php';
+$fields = array(
+	'postvar1' => 1,
+	'postvar2' => 2
+	
+);
+
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+
+//open connection
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"https://drawningbunny.azurewebsites.net/test_curl/2.php");
-curl_setopt($ch, CURLOPT_POST, 2);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "postvar1=1&postvar2=2");
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
 
-
-// receive server response ...
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec ($ch);
-echo $server_output."===";
-curl_close ($ch);
-
-// further processing ....
-if ($server_output == "OK") { echo "Ok"; } else { echo "Fail"; }
-
+//execute post
+$result = curl_exec($ch);
+echo $result."@@@@";
+//close connection
+curl_close($ch);
 ?>
